aa418e4f64460982ed3436d0d12f9f34e37d47c8
scalabrese <simoncalabrese94@gmail.com>
2020-05-02 10:22:05 +0200

    JewelsAndStones best performance

--- src/daily/JewelsAndStones.scala
+++ src/daily/JewelsAndStones.scala
@@ -1,8 +1,23 @@
 package daily
 
 object JewelsAndStones {
-  def numJewelsInStones(J: String, S: String): Int = {
+  def numJewelsInStones2(J: String, S: String): Int = {
     J.flatMap(j => S.map(s => if (s == j) 1 else 0)).sum
+  }
+
+  def numJewelsInStones(J: String, S: String): Int = {
+    S.map(s => if (J.contains(s)) 1 else 0).sum
+  }
+
+  def numJewelsInStonesReduce(J: String, S: String): Int = {
+    S.foldLeft(0)((acc, s) => if (J.contains(s)) acc + 1 else acc)
+  }
+
+  def numJewelsInStonesFor(J: String, S: String): Int = {
+    (for {
+      j <- J
+      s <- S if j == s
+    } yield 1).sum
   }
 
   def main(args: Array[String]): Unit = {
