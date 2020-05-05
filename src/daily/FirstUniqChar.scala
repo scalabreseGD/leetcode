47b02cf9593fa3b91cead61eb98ee0a22856c580
scalabrese <simoncalabrese94@gmail.com>
2020-05-05 14:12:57 +0200

    FirstUniqChar.scala

--- src/daily/FirstUniqChar.scala
+++ src/daily/FirstUniqChar.scala
@@ -3,7 +3,15 @@ package daily
 object FirstUniqChar {
 
   def firstUniqChar(s: String): Int = {
-    firstUniqChar656(s)
+    firstUniqChar604(s)
+  }
+
+  def firstUniqChar604(s: String): Int = {
+    s.map(char => (s.indexOf(char), s.lastIndexOf(char)))
+      .filter(elem => elem._1 != -1 && elem._1 == elem._2)
+      .sortBy(_._1)
+      .headOption
+      .map(_._1).getOrElse(-1)
   }
 
   def firstUniqChar656(s: String): Int = {
@@ -15,6 +23,7 @@ object FirstUniqChar {
       })
     if (data2 == Int.MaxValue) -1 else data2
   }
+
   def firstUniqChar690(s: String): Int = {
     val data = s.zipWithIndex
       .groupBy(_._1)
