15149b2c94b53da36024f2612d79a4590c13e78f
scalabrese <simoncalabrese94@gmail.com>
2020-05-12 16:01:44 +0200

    SingleNonDuplicate.scala

--- src/daily/week2/SingleNonDuplicate.scala
+++ src/daily/week2/SingleNonDuplicate.scala
@@ -14,12 +14,19 @@ object SingleNonDuplicate {
       }
     }
 
-    if(nums.length > 1) find(0, nums.length - 1)
+    if (nums.length > 1) find(0, nums.length - 1)
     else nums(0)
   }
 
   def main(args: Array[String]): Unit = {
-    println(singleNonDuplicate(Array(1,1,2,3,3,4,4,8,8)))
-    println(singleNonDuplicate(Array(3,3,7,7,10,11,11)))
+    val size = 10000
+    val useCase = (0 until size).flatMap(num => if (num == size / 2) Array(num) else Array(num, num)).toArray
+    val start = System.currentTimeMillis
+    println(singleNonDuplicate(useCase))
+    println(System.currentTimeMillis - start)
+
+
+    println(singleNonDuplicate(Array(1, 1, 2, 3, 3, 4, 4, 8, 8)))
+    println(singleNonDuplicate(Array(3, 3, 7, 7, 10, 11, 11)))
   }
 }
